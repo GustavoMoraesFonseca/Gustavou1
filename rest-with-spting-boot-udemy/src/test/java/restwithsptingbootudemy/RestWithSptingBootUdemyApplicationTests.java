76c76c889bbfb0fe3ae6e3d02187d5e40918b3d8
Gustavomoraesfonseca <you@example.com>
2020-07-22 14:52:53 -0300

    hello world

---
+++ rest-with-spting-boot-udemy/src/test/java/restwithsptingbootudemy/RestWithSptingBootUdemyApplicationTests.java
@@ -0,0 +1,13 @@
+package restwithsptingbootudemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSptingBootUdemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
